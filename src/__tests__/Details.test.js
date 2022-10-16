2e791e34737e31940d1ee833b16387bf2f9b2660
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-16 00:50:26 -0500

    Add Details component test

---
+++ src/__tests__/Details.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import Details from '../components/Details';
+
+test('Details test', async () => {
+  const details = render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <Details id="0" />
+        </Router>
+      </Provider>
+    </React.StrictMode>,
+  );
+  expect(details).toMatchSnapshot();
+});
