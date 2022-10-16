1f5a715cb8736b1ba93c012ef06c09336160ad2e
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-16 00:56:19 -0500

    Update snapshots

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
 import Navbar from '../components/Navbar';
 
 describe('Test for Navbar component', () => {
   test('Renders Navbar correctly', () => {
     const navbar = render(
-      <Router>
-        <Navbar />
-      </Router>,
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+        </Router>
+      </Provider>,
     );
     expect(navbar).toMatchSnapshot();
   });
