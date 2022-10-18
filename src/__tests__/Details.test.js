32859a2071ea0e4720f60265c2d3453b58d41f48
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-18 15:27:33 -0500

    Add Test for Details component

--- src/__tests__/Details.test.js
+++ src/__tests__/Details.test.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../redux/configureStore';
+import { createBrowserHistory } from 'history';
 import Details from '../components/Details';
+import store from '../redux/configureStore';
 
-test('Details test', async () => {
-  const details = render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <Router>
-          <Details />
-        </Router>
-      </Provider>
-    </React.StrictMode>,
-  );
-  expect(details).toMatchSnapshot();
+describe('Details test', () => {
+  test('snapshot for details', () => {
+    const history = createBrowserHistory();
+    const image = {
+      id: '-1',
+      author: 'Marco Oquendo',
+      width: 5616,
+      height: 3744,
+      url: './__snapshots__/testingImage.jpg',
+      downloadUrl: './__snapshots__/testingImage.jpg',
+      picsumUrl: './__snapshots__/testingImage.jpg',
+      nosizeUrl: './__snapshots__/testingImage.jpg',
+      blurUrl: './__snapshots__/testingImage.jpg',
+      grayUrl: './__snapshots__/testingImage.jpg',
+      grayBlurUrl: './__snapshots__/testingImage.jpg',
+    };
+
+    history.push('/imagedetail', { image });
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router history={history}>
+            <Routes>
+              <Route path="/coindetail" element={<Details />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
